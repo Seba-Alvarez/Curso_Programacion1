4c2d0a9dfaa30842e45efba64243e8dcc6ad473f
Seba <sebastianalvarez1998infor@gmail.com>
2025-07-26 13:42:52 -0300

    terminar_estos

---
+++ Desafíos/desafios_4_10_2.py
@@ -0,0 +1,4 @@
+#Desafío 2: Cambio Frecuente en Requisitos
+"""Supón que estás desarrollando un juego de video con distintos tipos de personajes y armas. 
+Los requerimientos cambian con frecuencia, añadiendo nuevos personajes y habilidades. 
+Mantener y actualizar TADs en este escenario podría ser una tarea titánica."""
---
+++ Desafíos/desafios_4_10_3.py
@@ -0,0 +1,5 @@
+#Desafío 3: Estructuras de Datos Anidadas
+"""Considera un sistema de manejo de inventario para una cadena de tiendas minoristas. 
+Tienes que tratar con datos de productos, tiendas, empleados, y transacciones, 
+donde cada tienda podría tener múltiples productos y empleados. 
+Gestionar estas relaciones con TADs podría ser ineficiente y propenso a errores."""
