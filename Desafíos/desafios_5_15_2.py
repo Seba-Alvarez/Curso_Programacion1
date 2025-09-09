bd1dbbb056fe15d7490e9a77ce06a06a010cb917
Seba <sebastianalvarez1998infor@gmail.com>
2025-09-09 11:24:27 -0300

    arboles

---
+++ Desafíos/desafios_5_15_2.py
@@ -0,0 +1,36 @@
+# Desafío 76: 
+#Construye un árbol binario simple con al menos 3 niveles de profundidad. 
+# Cada nodo debe contener un número entero como valor. 
+# Una vez construido el árbol, implementa una función que imprima los valores de 
+# los nodos siguiendo un recorrido en preorden.
+
+# Desafío 77: 
+#Dado un árbol binario con números enteros en cada nodo, implementa una función que recorra 
+# el árbol en inorden y calcule la suma de todos los valores almacenados en los nodos. 
+# El programa debe devolver el resultado final de la suma.
+
+# Desafío 78: 
+#Construye un árbol binario en el que cada nodo contiene un número entero. 
+# Implementa una función que recorra el árbol en postorden y devuelva el valor máximo 
+# encontrado entre todos los nodos del árbol.
+
+# Desafío 79: 
+#Dado un conjunto de números enteros únicos, construye un árbol de 
+# búsqueda binaria (ABB) que inserte los valores de manera que el subárbol 
+# izquierdo de cada nodo contenga solo nodos con valores menores, y el subárbol derecho 
+# solo nodos con valores mayores. Una vez construido el árbol, implementa una función 
+# para buscar un número dado y devuelva si ese número existe en el árbol o no.
+
+# Desafío 80:
+# Construir y evaluar un árbol de expresiones para una expresión matemática dada.
+#Tu tarea es escribir un programa en Python que haga lo siguiente:
+
+#Construir el Árbol de Expresiones: Dada una expresión matemática en forma de cadena, 
+# construir el árbol de expresiones correspondiente. Puedes asumir que la expresión está 
+# bien formada y solo contiene números enteros, y los operadores +, -, *, /.
+
+#Evaluar el Árbol de Expresiones: Una vez construido el árbol, 
+# evaluarlo y devolver el resultado de la expresión.
+
+#Prueba tu Programa: Utiliza la expresión "5 + 3 * 4" para probar tu programa. 
+# El resultado debería ser 17.
